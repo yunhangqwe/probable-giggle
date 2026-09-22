19d054d3e2879e5f9a915ada5cf8fc032b897973
Z yh <zyh@ZdeMacBook-Pro.local>
2026-09-22 09:27:37 +0800

    修复按天导航闪动

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nanchang-jingdezhen-guide-v2';
+const CACHE_NAME = 'nanchang-jingdezhen-guide-v3';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))));
